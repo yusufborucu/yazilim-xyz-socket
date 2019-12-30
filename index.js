022526a9f26547593f1ce59a59a65b6662685c71
Yusuf Borucu <yusufborucu@gmail.com>
2019-12-30 15:01:35 +0300

    user notify

---
+++ index.js
@@ -0,0 +1,36 @@
+const app = require('express')();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+
+http.listen(3000, () => {
+  console.log('Socket is running on port 3000.');
+});
+
+let users = [];
+
+io.on('connection', (socket) => {
+  socket.on('con_user_id', (data) => {
+    if (!users.includes(data)) {
+      users.push(data);
+    }
+  });
+
+  socket.on('que_user', (data) => {
+    if (users.includes('' + data.user_id + '')) {
+      let notify_user = {
+        user_id: data.user_id,
+        title: data.title
+      };
+      io.emit('notify_user', notify_user);
+    }
+  });
+
+  socket.on('dis_user_id', (data) => {
+    let index = users.indexOf(data);
+    if (index !== -1) users.splice(index, 1);
+  });
+
+  socket.on('disconnect', () => {
+
+  });
+});
