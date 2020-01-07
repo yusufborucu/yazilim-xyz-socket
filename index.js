e1892110b28d59982a7a058f5d8507247c2c1529
Yusuf Borucu <yusufborucu@gmail.com>
2020-01-07 10:42:18 +0300

    port

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const app = require('express')();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
+const PORT = process.env.PORT || 3000;
 
-http.listen(3000, () => {
+http.listen(PORT, () => {
   console.log('Socket is running on port 3000.');
 });
 
